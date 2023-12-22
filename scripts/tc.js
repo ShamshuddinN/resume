292cbcf0939b9a522402384c5709c205b50a49ad
Shamshuddin <nadafshamshu@gmail.com>
2023-12-22 18:17:45 +0530

    Improved error handling

--- scripts/tc.js
+++ scripts/tc.js
@@ -7,19 +7,27 @@ var isAlpha = function(ch){
 
 
 function inputSubmit() {
+  let mystr = '1 2 3 4 5 6 7 8 9 0'
   let iText = document.querySelector('#textInput').value;
   let incomingText = iText.replace(/\n/g, " ");
+
 
   // if (incomingText.includes('equipments') || incomingText.includes('Equipments')) {
   //   incomingText = incomingText.replace(/equipments/g, "equipment") 
   // }
 
   if (incomingText === '') {
+    document.querySelector('#textOutput').value = '';
     document.querySelector('#textOutput').placeholder = 'Please Enter Text in Input Text Box with a Line seperator.';
+    
     document.querySelector('#textOutput').disabled = true;
   } else if (isAlpha(incomingText[0])) {
     document.querySelector('#textOutput').placeholder = 'Error: No Line Seperator Found!';
     document.querySelector('#textOutput').disabled = true;
+  } else if (mystr.includes(incomingText[0])) {
+    document.querySelector('#textOutput').placeholder = 'Error: Numbered Line Seperators are not Supported yet.';
+    document.querySelector('#textOutput').disabled = true;
+
   } else {
     let sep = incomingText[0];
 
@@ -43,5 +51,6 @@ function inputSubmit() {
 // const str = "Mr Red has a red house and a red car";
 // const newText = str.replace(/red/g, "blue");
 // console.log(newText); // Mr Blue has a blue house and a blue car
+
 
 
