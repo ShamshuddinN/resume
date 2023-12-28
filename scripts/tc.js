f221cf39a445715ba75767ed9e34bbf35e8505f5
unknown <nadafshamshu@gmail.com>
2023-12-28 21:02:23 +0530

    Improved Script and Added New Feature

--- scripts/tc.js
+++ scripts/tc.js
@@ -1,37 +1,82 @@
 document.querySelector('#textOutput').disabled = true;
 
+function capitalizeWords(str) {
+  // convert the string to lowercase
+  str = str.toLowerCase();
+  // split the string into an array of words
+  let words = str.split(" ");
+  // loop through each word
+  for (let i = 0; i < words.length; i++) {
+    // get the first letter of the word
+    let firstLetter = words[i][0];
+    // capitalize the first letter
+    firstLetter = firstLetter.toUpperCase();
+    // replace the first letter of the word with the capitalized one
+    words[i] = firstLetter + words[i].slice(1);
+  }
+  // join the words back into a string
+  return words.join(" ");
+}
+
+
+function RefineString(rawStr) {
+  
+  if (typeof(rawStr) !== 'string') {
+    return 'No string Provided';
+  }
+
+  let spaces = rawStr.split(' ');
+  let sapceCount = spaces.length - 1;
+
+  if (rawStr.length <= 1 || sapceCount === rawStr.length) {
+    return 'Text Too Short or No Words found';
+  } else {
+    let refinedStr = '';
+    let incomingText = '';
+      if (rawStr.includes('\n')) {
+        incomingText = rawStr.replace(/\n/g, '');
+      } else {
+        incomingText = rawStr;
+      }
+
+    let raw = incomingText.split(' ');
+
+    for (let i = 0; i < raw.length; i++) {
+      if (raw[i] !== '') {
+        refinedStr += raw[i] + ' ';
+      }
+    }
+    return refinedStr;
+  }
+  
+}
+
 var isAlpha = function(ch){
   return /^[A-Z]$/i.test(ch);
 }
 
 
-
-function inputSubmit() {
+function inputSubmit(txtIn) {
   let mystr = '1 2 3 4 5 6 7 8 9 0'
-  let iText = document.querySelector('#textInput').value;
-  let incomingText = iText.replace(/\n/g, " ");
+  let inpTextHere = txtIn;
 
 
-  // if (incomingText.includes('equipments') || incomingText.includes('Equipments')) {
-  //   incomingText = incomingText.replace(/equipments/g, "equipment") 
-  // }
-
-  if (incomingText === '') {
+  if (inpTextHere === '') {
     document.querySelector('#textOutput').value = '';
     document.querySelector('#textOutput').placeholder = 'Please Enter Text in Input Text Box with a Line seperator.';
     
     document.querySelector('#textOutput').disabled = true;
-  } else if (isAlpha(incomingText[0]) ||incomingText[0] === ' ') {
+  } else if (isAlpha(inpTextHere[0]) ||inpTextHere[0] === ' ') {
     document.querySelector('#textOutput').placeholder = 'Error: No Line Seperator Found!';
     document.querySelector('#textOutput').disabled = true;
-  } else if (mystr.includes(incomingText[0])) {
+  } else if (mystr.includes(inpTextHere[0])) {
     document.querySelector('#textOutput').placeholder = 'Error: Numbered Line Seperators are not Supported yet.';
     document.querySelector('#textOutput').disabled = true;
 
   } else {
-    let sep = incomingText[0];
+    let sep = inpTextHere[0];
 
-    let out = incomingText.split(sep);
+    let out = inpTextHere.split(sep);
     let outText = ''
 
     for (let i = 1; i < out.length; i++) {
@@ -49,4 +94,29 @@ function inputSubmit() {
 }
 
 
+function inputHandle(event) {
+  
+  let PlainString = RefineString(event.target.value);
+  let trimmed = PlainString.trim()
+
+
+  if (event.shiftKey && event.key === 'Enter') {
+    document.querySelector('#textOutput').value = '';
+    inputSubmit(PlainString);
+
+  } else if (event.ctrlKey && event.key === 'Enter') {
+    document.querySelector('#textOutput').value = '';
+    let cOutput = capitalizeWords(trimmed);
+    
+    if (cOutput === 'Text Too Short Or No Words Found') {
+      document.querySelector('#textOutput').value = 'Text Too Short Or No Words Found';
+    } else {
+      document.querySelector('#textOutput').disabled = false;
+      document.querySelector('#textOutput').value = cOutput;
+    }
+
+  }
+
+
+};
 
